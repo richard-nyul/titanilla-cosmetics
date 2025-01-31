8932817803409c8173f7d73cf86ea79f8f765fab
Richárd Nyúl <odanyul@Richards-MacBook-Pro.local>
2025-01-31 23:42:52 +0100

    feat: setting up the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, 'src/assets'),
+      '@variables': resolve(__dirname, 'src/variables.css'),
+    },
+  },
+});
