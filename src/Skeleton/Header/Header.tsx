03dee222b53ebd224995cc3fafb7216678f44307
Richárd Nyúl <odanyul@Richards-MacBook-Pro.local>
2025-02-02 12:54:19 +0100

    feat: headermobile nav added

--- src/Skeleton/Header/Header.tsx
+++ src/Skeleton/Header/Header.tsx
@@ -1,30 +1,46 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './styles.css';
 import logo from '@assets/images/logo.png';
 import HeaderNav from '../HeaderNav/HeaderNav';
+import HeaderMobileNav from '../HeaderMobileNav/HeaderMobileNav';
 
 const Header = () => {
-  //   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  //   useEffect(() => {
-  //     const handleResize = () => setWindowWidth(window.innerWidth);
-  //     window.addEventListener('resize', handleResize);
-  //     return () => window.removeEventListener('resize', handleResize);
-  //   }, []);
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-  return (
-    <header className="header">
-      <div className="logo-container">
-        <NavLink className="mobile-header-nav-elem" to="/">
-          <img src={logo} alt="Logo" className="logo" />
-        </NavLink>
-      </div>
-      <div className="navigation-container">
-        <HeaderNav />
-      </div>
-    </header>
-  );
+  if (windowWidth > 1270) {
+    return (
+      <header className="header">
+        <div className="logo-container">
+          <NavLink className="mobile-header-nav-elem" to="/">
+            <img src={logo} alt="Logo" className="logo" />
+          </NavLink>
+        </div>
+        <div className="navigation-container">
+          <HeaderNav />
+        </div>
+      </header>
+    );
+  } else {
+    return (
+      <header className="header">
+        <div className="mobile-navigation-container">
+          <HeaderMobileNav />
+        </div>
+        <div className="logo-container">
+          <NavLink className="mobile-header-nav-elem" to="/">
+            <img src={logo} alt="Logo" className="logo" />
+          </NavLink>
+        </div>
+      </header>
+    );
+  }
 };
 
 export default Header;
---
+++ src/Skeleton/HeaderMobileNav/HeaderMobileNav.tsx
@@ -0,0 +1,115 @@
+import { useEffect, useRef, useCallback } from 'react';
+import { NavLink } from 'react-router-dom';
+import './styles.css';
+
+function HeaderMobileNav() {
+  const sidenavRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const overlayRef = useRef<HTMLDivElement>(null);
+
+  const closeNav = useCallback(() => {
+    if (sidenavRef.current) {
+      sidenavRef.current.classList.remove('show-sidenav');
+    }
+    setTimeout(() => {
+      if (overlayRef.current) {
+        overlayRef.current.style.display = 'none';
+      }
+    }, 200);
+  }, []);
+
+  function openNav() {
+    setTimeout(() => {
+      if (sidenavRef.current) {
+        sidenavRef.current.classList.toggle('show-sidenav');
+      }
+    }, 0);
+    if (overlayRef.current) {
+      overlayRef.current.style.display = 'block';
+    }
+  }
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        sidenavRef.current &&
+        !sidenavRef.current.contains(event.target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        closeNav();
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [closeNav]);
+
+  return (
+    <div>
+      <div id="overlay" ref={overlayRef}>
+        <div ref={sidenavRef} className="sidenav">
+          <button
+            type="button"
+            className="mobile-nav-button"
+            onClick={closeNav}
+          >
+            &times;
+          </button>
+          <div className="mobile-header-nav-container">
+            <NavLink
+              onClick={closeNav}
+              className="mobile-header-nav-elem"
+              to="/"
+            >
+              Kezdőlap
+            </NavLink>
+            <NavLink
+              onClick={closeNav}
+              className="mobile-header-nav-elem"
+              to="/services"
+            >
+              Szolgáltatások
+            </NavLink>
+            <NavLink
+              onClick={closeNav}
+              className="mobile-header-nav-elem"
+              to="/pricing"
+            >
+              Árlista
+            </NavLink>
+            <NavLink
+              onClick={closeNav}
+              className="mobile-header-nav-elem"
+              to="/contact"
+            >
+              Kapcsolat
+            </NavLink>
+            <NavLink
+              onClick={closeNav}
+              className="mobile-header-nav-elem mobile-booking"
+              to="/booking"
+            >
+              Időpontfoglalás
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button
+          ref={buttonRef}
+          className="mobile-nav-button"
+          type="button"
+          onClick={openNav}
+        >
+          &#9776;
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderMobileNav;
