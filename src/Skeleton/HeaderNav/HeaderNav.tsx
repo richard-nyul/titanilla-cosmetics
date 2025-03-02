51dddd6cfed0b6d3a67f191d1639f84640809424
Richárd Nyúl <odanyul@Richards-MacBook-Pro.local>
2025-03-02 21:46:25 +0100

    feat: scrolldown to header added

--- src/Skeleton/HeaderNav/HeaderNav.tsx
+++ src/Skeleton/HeaderNav/HeaderNav.tsx
@@ -5,18 +5,45 @@ import useGlow from '../../hooks/useGlow';
 const HeaderNav = () => {
   const bookingRef = useGlow<HTMLAnchorElement>();
 
+  const handleScroll = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    sectionId: string
+  ) => {
+    e.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="header-nav-container">
-      <NavLink className="header-nav-elem basic" to="/">
+      <NavLink
+        className="header-nav-elem basic"
+        to="/"
+        onClick={(e) => handleScroll(e, 'home')}
+      >
         Kezdőlap
       </NavLink>
-      <NavLink className="header-nav-elem basic" to="/services">
+      <NavLink
+        className="header-nav-elem basic"
+        to="/#services"
+        onClick={(e) => handleScroll(e, 'services')}
+      >
         Szolgáltatások
       </NavLink>
-      <NavLink className="header-nav-elem basic" to="/pricing">
+      <NavLink
+        className="header-nav-elem basic"
+        to="/#pricing"
+        onClick={(e) => handleScroll(e, 'pricing')}
+      >
         Árlista
       </NavLink>
-      <NavLink className="header-nav-elem basic" to="/contact">
+      <NavLink
+        className="header-nav-elem basic"
+        to="/#contact"
+        onClick={(e) => handleScroll(e, 'contact')}
+      >
         Kapcsolat
       </NavLink>
       <NavLink
