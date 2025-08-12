e5bb12287af6c779c1d5bf781485f3fe662aa2b4
Richárd Nyúl <odanyul@Richards-MacBook-Pro.local>
2025-08-12 15:34:35 +0200

    feat: tablist implemented

--- src/sections/ServiceDetails/ServiceDetails.tsx
+++ src/sections/ServiceDetails/ServiceDetails.tsx
@@ -1,11 +1,21 @@
 import './styles.scss';
+import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { services } from '../../constants/constants';
+import { useNavigate } from 'react-router-dom';
 
 const ServiceDetails = () => {
   const { name } = useParams<{ name: string }>();
   const decodedName = decodeURIComponent(name || '');
+  const activeIndex = services.findIndex((s) => s.name === decodedName);
   const service = services.find((s) => s.name === decodedName);
+  const navigate = useNavigate();
+
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   if (!service) {
     return (
@@ -16,44 +26,78 @@ const ServiceDetails = () => {
     );
   }
 
+  const indicatorIndex = hoveredIndex !== null ? hoveredIndex : activeIndex;
+
   return (
     <div className="service-detail-container">
-      <div className="service-detail-content-container">
-        <div className="service-detail-text">
-          <h1>{service.name}</h1>
-          <p>{service.details}</p>
-          <p>{service.treatment}</p>
+      <div className="service-detail-subcontainer">
+        <ul className="tabs-nav" role="tablist">
+          {services.map((s, i) => (
+            <li
+              onClick={() =>
+                navigate(`/services/${encodeURIComponent(s.name)}`)
+              }
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') {
+                  e.preventDefault();
+                  navigate(`/services/${encodeURIComponent(s.name)}`);
+                }
+              }}
+              key={s.name}
+              role="tab"
+              tabIndex={s.name === name ? 0 : -1}
+              aria-selected={s.name === name}
+              className={`tab-item ${s.name === name ? 'active' : ''}`}
+              onMouseEnter={() => setHoveredIndex(i)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              <Link to={`/services/${encodeURIComponent(s.name)}`}>
+                {s.name}
+              </Link>
+            </li>
+          ))}
+          <li
+            className="tab-line"
+            style={{
+              height: `${100 / services.length}%`,
+              transform: `translateY(${indicatorIndex * 100}%)`,
+            }}
+          />
+        </ul>
 
-          <h3>Előnyök</h3>
-          <ul>
-            {service.pros.map((pro, i) => (
-              <li key={i}>{pro}</li>
-            ))}
-          </ul>
+        <div className="service-detail-content-container">
+          <div className="service-detail-text">
+            <h1>{service.name}</h1>
+            <p>{service.details}</p>
+            <p>{service.treatment}</p>
 
-          <h3>Ellenjavallatok</h3>
-          <ul>
-            {service.cons.map((con, i) => (
-              <li key={i}>{con}</li>
-            ))}
-          </ul>
+            <h3>Előnyök</h3>
+            <ul>
+              {service.pros.map((pro, i) => (
+                <li key={i}>{pro}</li>
+              ))}
+            </ul>
 
-          <h3>Eredmény</h3>
-          <p>{service.result}</p>
+            <h3>Ellenjavallatok</h3>
+            <ul>
+              {service.cons.map((con, i) => (
+                <li key={i}>{con}</li>
+              ))}
+            </ul>
+
+            <h3>Eredmény</h3>
+            <p>{service.result}</p>
+          </div>
+
+          <img
+            loading="lazy"
+            src={'/images/services/botorx.webp'}
+            alt={service.name}
+          />
         </div>
-        {/* <img
-        src={`/images/services/${service.image}`}
-        alt={service.name}
-        className="service-image"
-        /> */}
-        <img
-          loading="lazy"
-          src={'/images/services/botorx.webp'}
-          alt={service.name}
-        />
       </div>
-      <Link to="/services" className="back-link">
-        Vissza
+      <Link to="/" className="back-link">
+        ← Vissza
       </Link>
     </div>
   );
